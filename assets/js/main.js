1d6fcba76a702db625724896d6f8fd430e8da613
Sam Li <sam@latis.co>
2019-06-14 16:56:58 +0000

    Deploy from Forestry.io

--- assets/js/main.js
+++ assets/js/main.js
@@ -345,10 +345,10 @@ $('.nav__link_request_access').on('click', function(e){
 });
 
   const scriptURL = 'https://script.google.com/macros/s/AKfycby3LHSmO14heLdbkEL2QGSHrTLGXfq2-aFx5ksZlWcZ3TLM6TRP/exec'
-  const scripttURL = 'https://script.google.com/macros/s/AKfycby3LHSmO14heLdbkEL2QGSHrTLGXfq2-aFx5ksZlWcZ3TLM6TRP/exec'
+  //const scripttURL = 'https://script.google.com/macros/s/AKfycby3LHSmO14heLdbkEL2QGSHrTLGXfq2-aFx5ksZlWcZ3TLM6TRP/exec'
 
   const form = document.forms['submit-to-google-sheet']
-  const formt = document.forms['submit-to-google-sheett']
+  //const form = document.forms['submit-to-google-sheett']
 
   form.addEventListener('submit', e => {
   	console.log('1');
@@ -358,13 +358,13 @@ $('.nav__link_request_access').on('click', function(e){
       .catch(error => console.error('Error!', error.message))
   })
 
-  formt.addEventListener('submit', e => {
+  /*formt.addEventListener('submit', e => {
     console.log('2');
     e.preventDefault()
     fetch(scripttURL, { method: 'POST', body: new FormData(form)})
       .then(response => console.log('Success!', response))
       .catch(error => console.error('Error!', error.message))
-  })
+  })*/
 
 
 });
