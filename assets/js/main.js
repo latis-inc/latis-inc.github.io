458ac0fc39b2fbe22e8287c41e83a7c847fcb04e
Dima <chermanderX@yandex.ru>
2019-06-18 12:11:35 +0300

    12/11

--- assets/js/main.js
+++ assets/js/main.js
@@ -399,7 +399,8 @@ if(str.search(/blog/i) != -1){
     itemPageBlog.innerHTML = itemPageBlog.innerHTML + '<div class="section__post-blog" style="padding: 0; margin: 0; box-shadow: none"></div>';
   }
 }
-  document.getElementsByClassName('nav__link_blog')[0].style.color = '#f80000';
+  document.getElementsByClassName('nav__link_blog')[0].style.borderBottom = '2px solid #321A81';
+  document.getElementsByClassName('nav__link_blog')[0].style.paddingBottom = '4px';
 }
 
 });
